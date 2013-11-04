28fba389d2d606c847517bedd0587d743ee911a4
Adam Richman <ar@adamrichman.com>
2013-11-03 19:21:12 -0500

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,10 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+  	it "should have the right title" do
+  		visit '/static_pages/home'
+  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+  	end    
   end
 
   describe "Help page" do
@@ -15,12 +19,20 @@ describe "StaticPages" do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
   	end
+  	it "should have the right title" do
+  		visit '/static_pages/help'
+  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+  	end  	
   end
 
   describe "About page" do 
   	it "should have the content 'About us'" do
   		visit '/static_pages/about'
   		expect(page).to have_content('About us')
+  	end
+  	it "should have the right title" do
+  		visit '/static_pages/about'
+  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
   	end
   end
 end
