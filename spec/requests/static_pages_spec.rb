498add0af478a9dc2cc70364cd896df456d19db8
Adam Richman <ar@adamrichman.com>
2013-11-03 19:54:10 -0500

    Utilize rspec 'let' function to DRY code

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+	let(:base_title) { 'Ruby on Rails Tutorial Sample App'}
 
   describe "Home page" do
     
@@ -10,7 +11,7 @@ describe "StaticPages" do
     end
   	it "should have the right title" do
   		visit '/static_pages/home'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+  		expect(page).to have_title("#{base_title} | Home")
   	end    
   end
 
@@ -21,7 +22,7 @@ describe "StaticPages" do
   	end
   	it "should have the right title" do
   		visit '/static_pages/help'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+  		expect(page).to have_title("#{base_title} | Help")
   	end  	
   end
 
@@ -32,7 +33,7 @@ describe "StaticPages" do
   	end
   	it "should have the right title" do
   		visit '/static_pages/about'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+  		expect(page).to have_title("#{base_title} | About Us")
   	end
   end
   
@@ -43,7 +44,7 @@ describe "StaticPages" do
   	end
   	it "should have the right title 'Contact Us'" do
   		visit '/static_pages/contact'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
+  		expect(page).to have_title("#{base_title} | Contact Us")
   	end
   end  
 end
